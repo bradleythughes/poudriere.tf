3b4cf51227e99e6019699f6da209bab69ce49963
Bradley T. Hughes <bradleythughes@appear.in>
2018-03-05 21:23:16 +0100

    Add a security group to allow SSH/HTTP into EC2 instances
    Instances running poudriere need ssh for control and HTTP access for the
    poudriere webapp.
    
    aws_security_group.poudriere: Creating...
      description:                           "" => "Allow SSH/HTTP from the internet"
      egress.#:                              "" => "<computed>"
      ingress.#:                             "" => "4"
      ingress.1010096078.cidr_blocks.#:      "" => "0"
      ingress.1010096078.description:        "" => ""
      ingress.1010096078.from_port:          "" => "22"
      ingress.1010096078.ipv6_cidr_blocks.#: "" => "1"
      ingress.1010096078.ipv6_cidr_blocks.0: "" => "::/0"
      ingress.1010096078.protocol:           "" => "tcp"
      ingress.1010096078.security_groups.#:  "" => "0"
      ingress.1010096078.self:               "" => "false"
      ingress.1010096078.to_port:            "" => "22"
      ingress.2214680975.cidr_blocks.#:      "" => "1"
      ingress.2214680975.cidr_blocks.0:      "" => "0.0.0.0/0"
      ingress.2214680975.description:        "" => ""
      ingress.2214680975.from_port:          "" => "80"
      ingress.2214680975.ipv6_cidr_blocks.#: "" => "0"
      ingress.2214680975.protocol:           "" => "tcp"
      ingress.2214680975.security_groups.#:  "" => "0"
      ingress.2214680975.self:               "" => "false"
      ingress.2214680975.to_port:            "" => "80"
      ingress.2541437006.cidr_blocks.#:      "" => "1"
      ingress.2541437006.cidr_blocks.0:      "" => "0.0.0.0/0"
      ingress.2541437006.description:        "" => ""
      ingress.2541437006.from_port:          "" => "22"
      ingress.2541437006.ipv6_cidr_blocks.#: "" => "0"
      ingress.2541437006.protocol:           "" => "tcp"
      ingress.2541437006.security_groups.#:  "" => "0"
      ingress.2541437006.self:               "" => "false"
      ingress.2541437006.to_port:            "" => "22"
      ingress.4051041281.cidr_blocks.#:      "" => "0"
      ingress.4051041281.description:        "" => ""
      ingress.4051041281.from_port:          "" => "80"
      ingress.4051041281.ipv6_cidr_blocks.#: "" => "1"
      ingress.4051041281.ipv6_cidr_blocks.0: "" => "::/0"
      ingress.4051041281.protocol:           "" => "tcp"
      ingress.4051041281.security_groups.#:  "" => "0"
      ingress.4051041281.self:               "" => "false"
      ingress.4051041281.to_port:            "" => "80"
      name:                                  "" => "poudriere"
      owner_id:                              "" => "<computed>"
      revoke_rules_on_delete:                "" => "false"
      vpc_id:                                "" => "vpc-620dc60b"
    aws_security_group.poudriere: Creation complete after 3s (ID: sg-0cfbcc9b3db4f3b73)
    
    Apply complete! Resources: 1 added, 0 changed, 0 destroyed.

--- main.tf
+++ main.tf
@@ -19,3 +19,41 @@ resource aws_key_pair ssh-key {
   key_name = "${var.public_key_name}"
   public_key = "${file(local.public_key_filename)}"
 }
+
+data aws_vpc default {
+  default = true
+}
+
+resource aws_security_group poudriere {
+  name = "poudriere"
+  description = "Allow SSH/HTTP from the internet"
+  vpc_id = "${data.aws_vpc.default.id}"
+
+  ingress {
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
